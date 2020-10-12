8b0987f758e159fd6092c055e74fc67e322dbc9f
yefanjiang <yefanjiang@foxmail.com>
2020-10-12 22:07:35 +0800

    day01over

---
+++ demo/src/main/java/com/example/demo/Config/MybatisPlusConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.Config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.example.demo.Mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(100);//ms，超过此处设置的ms则sql不执行
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+}
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.Mapper.UserMapper;
 import com.example.demo.Bean.User;
 import org.junit.jupiter.api.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.awt.*;
 import java.util.List;
 
 @SpringBootTest
@@ -25,10 +28,37 @@ class DemoApplicationTests {
 	@Test
 	public void addUser() {
 		User user = new User();
-		user.setAge(23);
-		user.setName("jyf");
-		user.setEmail("yefanjiang@foxmail.com");
+		user.setName("222");
+		user.setEmail("222@foxmail.com");
+		//user.setId(6L);
 		userMapper.insert(user);
+	}
+
+	@Test
+	public void updateUser() {
+		User user = userMapper.selectById(1315619263014305793L);
+		user.setAge(118);
+		user.setVersion(user.getVersion() - 1);
+		userMapper.updateById(user);
+	}
+
+	@Test
+	public void selectPage() {
+		Page<User> page = new Page<>(1, 5);
+		userMapper.selectPage(page, null);
+
+		page.getRecords().forEach(System.out::println);
+		System.out.println(page.getCurrent());
+		System.out.println(page.getPages());
+		System.out.println(page.getSize());
+		System.out.println(page.getTotal());
+		System.out.println(page.hasNext());
+		System.out.println(page.hasPrevious());
+	}
+
+	@Test
+	public void logicalDelete() {
+		System.out.println(userMapper.deleteById(6L));
 	}
 
 }
